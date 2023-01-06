354bc05841f8f8f84cd2553fc02aa4222b1b50c5
Yogesh Singh <yogesh.singh.2810@gmail.com>
2023-01-06 18:09:39 +0530

    updated starter kit

--- lib/screens/message_screen.dart
+++ lib/screens/message_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:hmssdk_flutter/hmssdk_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:telehealth_app/models/message.dart';
